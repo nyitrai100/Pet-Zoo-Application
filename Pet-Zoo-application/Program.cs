8d18b2883bd150e2f4053e79dc4641c3cc7cd5ac
nyitraibence <nyitrai100@gmail.com>
2024-03-18 15:11:50 +0000

    petZooArray

--- Pet-Zoo-application/Program.cs
+++ Pet-Zoo-application/Program.cs
@@ -1,4 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("hello World!");
+string[] pettingZoo = 
+{
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
